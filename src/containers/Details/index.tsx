48771ddfd39706061283471cee248c2049ab1f78
Willane Paiva <willanepaiva@hotmail.com>
2019-06-26 23:11:21 -0300

    Add details as home sections

---
+++ src/containers/Details/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from '../../components/Section';
+
+const Details = () => (
+  <>
+    <Section title="What is?" description="Insert something" />
+    <Section title="Getting Started" description="Insert something" />
+    <Section title="Contributing" description="Insert something" />
+  </>
+);
+
+export default Details;
