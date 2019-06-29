cab4750f9b35599ef3ba00e6da203af8d0e2a9fb
Willane Paiva <willanepaiva@hotmail.com>
2019-06-29 13:53:18 -0300

    Modify social media component to accept link and title

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -5,10 +5,21 @@ import './style.scss';
 interface OwnProps {
   path: string;
   desc: string;
+  link: string;
+  title: string;
 }
 
-const SocialMedia: FunctionComponent<OwnProps> = ({ path, desc }) => (
-  <img className="social-media" src={path} alt={desc} />
+const SocialMedia: FunctionComponent<OwnProps> = ({
+  path,
+  desc,
+  link,
+  title,
+}) => (
+  <div className="social-media">
+    <a href={link} title={title}>
+      <img src={path} alt={desc} />
+    </a>
+  </div>
 );
 
 export default SocialMedia;
