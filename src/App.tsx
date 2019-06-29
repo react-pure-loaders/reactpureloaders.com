9759320aed5da48dd02139d1ab942ea7348f599e
Willane Paiva <willanepaiva@hotmail.com>
2019-06-29 17:29:19 -0300

    Remove Details Container and update to Home container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Home from './containers/Home';
 
 function App() {
   return (
     <div className="App">
-      content
+      <Home />
     </div>
   );
 }
--- src/stories/Body.stories.tsx
+++ src/stories/Body.stories.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react';
 
 import Section from '../components/Section';
-import Details from '../containers/Details'
+import Home from '../containers/Home';
 
 const BodyStories = storiesOf('Body', module)
 
-BodyStories.add('Section' ,() => <Section title='What it is?' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper eget duis at tellus at.' />).add('Details', () => <Details /> );
+BodyStories.add('Section' ,() => <Section title='What it is?' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper eget duis at tellus at.' />).add('Home Sections', () => <Home />);
