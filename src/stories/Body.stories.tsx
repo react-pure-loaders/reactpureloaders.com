e4a1e361a0b1f48b71fe4a37967dd29222a2ba0e
Willane Paiva <willane.paiva@codeminer42.com>
2019-08-28 16:08:54 -0300

    Update stories

--- src/stories/Body.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { storiesOf } from '@storybook/react';
-
-import Section from '../components/Section';
-
-const BodyStories = storiesOf('Body', module)
-
-BodyStories.add('Section' ,() => <Section title='What it is?' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper eget duis at tellus at.' />);
